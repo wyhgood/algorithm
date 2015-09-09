57b79048b6b32f54b5fdfb925ea0a1d03c8952ef
Wangzi <756651720@qq.com>
2015-09-08 21:50:42 -0700

    wrote

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
-{
-  cout<< "hello world" << endl;
+{ 
+  vector<int> v;
+
+  int numbers;
+  for(int i=0; i<=5; i++){
+    cin >> numbers;
+    v.push_back(numbers);
+  }
+  //cin >> numbers;
+  
+  cout<< "hello world\t" << v.size() <<endl;
 }
