8777089d571f08a757963fec085990867764c37f
Wangzi <756651720@qq.com>
2015-09-09 00:15:16 -0700

    a complete sort , worked

--- insertion_sort.c
+++ insertion_sort.c
@@ -13,5 +13,21 @@ int main()
   }
   //cin >> numbers;
   
-  cout<< "hello world\t" << v.size() <<endl;
+  cout<< "v.size-----\t" << v.size() <<endl;
+  int temp;
+  int exchange;
+  for(int i=1; i<=5; i++){
+    temp = i;
+    while(temp-1>=0 && v[temp] <= v[temp-1]){
+      exchange = v[temp-1];
+      v[temp-1] = v[temp];
+      v[temp] = exchange;
+      temp--;
+    }
+    
+  }
+  for(int i=0; i<=5; i++){
+    cout<< v[i] << endl;
+  }
+  return 0;
 }
